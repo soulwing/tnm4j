79b832f481160156b1c360a21d07e6082ab40106
Carl Harris <ceharris@vt.edu>
2015-04-01 05:50:01 -0400

    added missing logger class

---
+++ src/main/java/org/soulwing/snmp/provider/snmp4j/Snmp4jLogger.java
@@ -0,0 +1,35 @@
+/*
+ * tnm4j - Simplified SNMP API for Java
+ * Copyright (C) 2012 Carl Harris, Jr
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.soulwing.snmp.provider.snmp4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * A static logger for this package.
+ *
+ * @author Carl Harris
+ */
+class Snmp4jLogger {
+
+  public static final Logger logger = LoggerFactory.getLogger(
+      Snmp4jLogger.class.getPackage().getName());
+  
+}
